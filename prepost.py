84911ea03760b4120c9ebf981efb98b535989362
shinshu68 <shinshu68@gmail.com>
2019-11-03 10:25:47 +0900

    :recycle: 処理を関数に分割する

--- prepost.py
+++ prepost.py
@@ -2,15 +2,29 @@ import os
 import json
 import toml
 
-config_file = ''
-if os.getenv('XDG_CONFIG_HOME'):
-    config_file = os.path.join(os.getenv('XDG_CONFIG_HOME'), 'prepost', 'config.toml')
-else:
-    config_file = os.path.join(os.getenv('HOME'), '.config', 'prepost', 'config.toml')
 
-if not os.path.exists(config_file):
-    print('config file is not exists.')
+def get_config_file_path():
+    config_file_path = ''
+    if os.getenv('XDG_CONFIG_HOME'):
+        config_file_path = os.path.join(os.getenv('XDG_CONFIG_HOME'), 'prepost', 'config.toml')
+    else:
+        config_file_path = os.path.join(os.getenv('HOME'), '.config', 'prepost', 'config.toml')
 
-with open(config_file) as f:
-    config = toml.load(f)
-    print(json.dumps(config, indent=4, sort_keys=True, separators=(',', ': ')))
+    if not os.path.exists(config_file_path):
+        print('config file is not exists.')
+
+    return config_file_path
+
+
+def main():
+    config_file = get_config_file_path()
+    if config_file == "":
+        exit()
+
+    with open(config_file) as f:
+        config = toml.load(f)
+        print(json.dumps(config, indent=4, sort_keys=True, separators=(',', ': ')))
+
+
+if __name__ == '__main__':
+    main()
