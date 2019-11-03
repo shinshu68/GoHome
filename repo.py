c80e73efc86795da9257985e3078bb1351e67552
shinshu68 <shinshu68@gmail.com>
2019-11-03 17:46:00 +0900

    :sparkles: テストを満たすpre post関数を作成

--- repo.py
+++ repo.py
@@ -4,7 +4,7 @@ import os
 
 class repo():
     def __init__(self, path, pre=None, post=None):
-        self.path = path
+        self.path = str(Path(path).expanduser())
         self._pre = pre
         self._post = post
 
@@ -22,13 +22,27 @@ class repo():
         return self._post
 
     def pre(self):
-        os.chdir(Path(self.path).expanduser())
-        print(os.getcwd())
-        for item in self.get_pre():
-            print(item)
+        if not os.path.exists(self.path):
+            # print(self.path)
+            pass
+        # os.chdir(Path(self.path).expanduser())
+        # print(os.getcwd())
+
+        pre = self.get_pre()
+        if 'branch' not in pre:
+            raise ValueError
+        if 'check' not in pre or len(pre['check']) == 0:
+            raise ValueError
 
     def post(self):
-        os.chdir(Path(self.path).expanduser())
-        print(os.getcwd())
-        for item in self.get_post():
-            print(item)
+        if not os.path.exists(self.path):
+            # print(self.path)
+            pass
+        # os.chdir(Path(self.path).expanduser())
+        # print(os.getcwd())
+
+        post = self.get_post()
+        if 'branch' not in post:
+            raise ValueError
+        if 'check' not in post or len(post['check']) == 0:
+            raise ValueError
