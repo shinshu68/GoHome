d88b6942381f796cd59c7c8d4710e3dda6744cd4
shinshu68 <shinshu68@gmail.com>
2019-11-08 17:02:16 +0900

    :sparkles: 引数に応じてpreかpostのcommandsをexecuteするように変更

--- prepost.py
+++ prepost.py
@@ -20,7 +20,7 @@ def get_config_file_path():
     return config_file_path
 
 
-def main(mode='pre'):
+def main(mode):
     config_file = get_config_file_path()
 
     with open(config_file) as f:
@@ -29,21 +29,32 @@ def main(mode='pre'):
     # print(json.dumps(config, indent=4, sort_keys=True, separators=(',', ': ')))
     # exit()
 
-    pre_list = []
-    for repo in config.get('pre').get('repo'):
-        pre_list.append(Repo.repo(repo.get('path'), repo.get('data')))
+    if mode == 'pre':
+        pre_list = []
+        for repo in config.get('pre').get('repo'):
+            pre_list.append(Repo.repo(repo.get('path'), repo.get('data')))
 
-    post_list = []
-    for repo in config.get('post').get('repo'):
-        post_list.append(Repo.repo(repo.get('path'), repo.get('data')))
+        for pre in pre_list:
+            print(pre.execute())
 
-    for pre in pre_list:
-        print(pre.execute())
+    if mode == 'post':
+        post_list = []
+        for repo in config.get('post').get('repo'):
+            post_list.append(Repo.repo(repo.get('path'), repo.get('data')))
 
-    for post in post_list:
-        print(post.execute())
+        for post in post_list:
+            print(post.execute())
 
 
 if __name__ == '__main__':
     argv = sys.argv
-    main()
+    if len(argv) != 2:
+        print('Usage: python prepost.py {pre|post}')
+        exit()
+
+    mode = argv[1]
+    if mode != 'pre' and mode != 'post':
+        print('Usage: python prepost.py {pre|post}')
+        exit()
+
+    main(mode)
