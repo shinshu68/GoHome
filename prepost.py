d56b51da23347fc9db9ff4684e15aaf02a5b1f8f
shinshu68 <shinshu68@gmail.com>
2019-11-09 21:57:54 +0900

    :recycle: 実行するのはpreかpostの二択なのでまとめられる

--- prepost.py
+++ prepost.py
@@ -21,29 +21,18 @@ def get_config_file_path():
 
 
 def main(mode):
-    config_file = get_config_file_path()
-
-    with open(config_file) as f:
+    with open(get_config_file_path()) as f:
         config = toml.load(f)
 
     # print(json.dumps(config, indent=4, sort_keys=True, separators=(',', ': ')))
     # exit()
 
-    if mode == 'pre':
-        pre_list = []
-        for repo in config.get('pre').get('repo'):
-            pre_list.append(Repo.repo(repo.get('path'), repo.get('data')))
+    item_list = []
+    for repo in config.get(mode).get('repo'):
+        item_list.append(Repo.repo(repo.get('path'), repo.get('data')))
 
-        for pre in pre_list:
-            print(pre.get_path(), pre.execute())
-
-    if mode == 'post':
-        post_list = []
-        for repo in config.get('post').get('repo'):
-            post_list.append(Repo.repo(repo.get('path'), repo.get('data')))
-
-        for post in post_list:
-            print(post.get_path(), post.execute())
+    for item in item_list:
+        print(item.get_path(), item.execute())
 
 
 if __name__ == '__main__':
